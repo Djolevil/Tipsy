0d6fdc6e042723705af8a5010bf1b725e4c26c70
Djordje Knezevic <starwarrior@mycity.rs>
2022-11-08 18:08:32 +0100

    Create new file ResultsViewController.swift, link it with Results screen, create IBOutlets and IBActions for its UI components

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Djordje Knezevic on 8.11.22..
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
